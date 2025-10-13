e20f985bfd7477f737a96bc778a61b6ffc770926
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-10-13 23:19:44 +0200

    added JS code
    console.log added
    diff way of variables added
    operators and strings added

---
+++ script/index.js
@@ -0,0 +1,53 @@
+//this is exercise1
+
+console.log("Hello World");
+alert("Welcome to JavaScript World");
+prompt("welcome back")
+
+//this is exercise 2
+let fullName= "Sonal Maheshwari";
+console.log(`My name is: ${fullName}`);
+
+let age= 30;
+console.log(`my age is: ${age}`);
+
+let favfruit ="apple";
+console.log(`My favourite fruit is: ${favfruit}`);
+
+
+//this is exercise 3
+
+let students= 10;
+ console.log(students);
+ console.log(typeof students);
+
+ //students = students + 1;
+ //students = students - 2;
+// students = students * 2;
+// students = students / 2;
+
+//another way -
+//students += 1;
+//students -= 5;
+//students *= 4;
+//students /= 2;
+//console.log(students);
+
+
+let price= 500;
+price =price + 40;
+console.log(`the total price is: ${price}`);
+
+let CGPA=8.2;
+CGPA -= 2;
+console.log(`The CGPA of Max is :${CGPA}`);
+
+let bankno= 78908;
+bankno = bankno * 3;
+console.log(`The banknumber is: ${bankno}`);
+
+let rating= 5;
+rating ++;
+//rating --;
+console.log(`the rating of show is :${rating}`);
+ 
