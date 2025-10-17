a9790a61b69ea834e28f6571f2162db92dcb4375
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-10-17 12:30:31 +0200

    Update JS2.js

--- script/JS2.js
+++ script/JS2.js
@@ -1,8 +1,8 @@
  let priceInput = prompt("Enter a price (for eg: $100):");
  console.log(priceInput);
 
- let price = parseFloat(priceInput.replace("$",""));
- 
+ let price = parseFloat(priceInput.replace("$","")); //we can use slice here or replace would works better ? 
+
  let percentReduce = price * 0.1;
 
  let finalPrice = price - percentReduce;
