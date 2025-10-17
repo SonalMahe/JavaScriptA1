d3d56dd1761d3e29811c1c480a9582526ec8be26
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-10-17 12:25:09 +0200

    make changes
    added replace method

--- script/JS2.js
+++ script/JS2.js
@@ -1,11 +1,11 @@
- let price = prompt("Enter a price here:");
- console.log("$"+price);
+ let priceInput = prompt("Enter a price (for eg: $100):");
+ console.log(priceInput);
 
- let priceInNumber = parseInt(price);
+ let price = parseFloat(priceInput.replace("$",""));
+ 
+ let percentReduce = price * 0.1;
 
- let percentReduce = priceInNumber * 0.1;
-
- let finalPrice = priceInNumber - percentReduce;
+ let finalPrice = price - percentReduce;
 
  console.log("Your new price is: ", "$"+finalPrice);
  
