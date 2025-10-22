3657253c6f7b85aea8ef5a60d11da8e7571a13c5
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-10-22 21:47:33 +0200

    Changes added for username not contains in password

--- script/JS3.js
+++ script/JS3.js
@@ -4,17 +4,17 @@ let password = prompt("Enter password");
 function IsValidPassword(username, password) {
     if (password.length < 8) //password length should be min 8 char.
     {
-        console.log('password length should be min 8 characters');
+        console.log('password length should be min 8 characters.');
         return false;
     }
     if (password.indexOf(" ") !== -1) //password can not contain spaces.
     {
-        console.log('password should not contain any space');
+        console.log('password should not contain any space.');
         return false;
     }
-    if (password == username) //can not contain username
+    if (password.includes(username)) //can not contain username
     {
-        console.log('password should not be same as username');
+        console.log('password should not contain username.');
         return false;
     }
     return true;
